2c0ef53f07528481f9227aef2d42dab2822038f3
Soumili Basu <115858327+soumilibasu@users.noreply.github.com>
2025-03-15 12:33:54 +0530

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -4,7 +4,7 @@ os.system("pip install --no-cache-dir --upgrade scikit-learn")
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score
+from sklearn.multioutput import MultiOutputClassifier
 
 import streamlit as st
 import pandas as pd
